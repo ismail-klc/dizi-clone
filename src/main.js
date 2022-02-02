0f04471acedd0470bb8a7cd849f2f7d3ba4f3569
Ismail Kılıç <ismail000728@gmail.com>
2022-02-02 13:17:43 +0300

    pwa options updated.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './assets/index.css'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+
 const app = createApp(App)
 
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-  vue(),
-  VitePWA({
-    manifest: {
-      name: 'TV Maze',
-      short_name: 'TV Maze',
-      icons: [
-        {
-          src: '/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    },
-  }),]
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      workbox: {
+        sourcemap: true
+      },
+      strategies: 'injectManifest',
+      manifest: {
+        name: 'TV Maze',
+        short_name: 'TV Maze',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),]
 })
