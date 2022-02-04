fa3e093dbf6235418d5b675381b9a511c809e922
Ismail Kılıç <ismail000728@gmail.com>
2022-02-05 00:54:03 +0300

    pwa removed.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import './assets/index.css'
 
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-})
+// const updateSW = registerSW({
+//   onNeedRefresh() {},
+//   onOfflineReady() {},
+// })
 
 const app = createApp(App)
 
