d86998ab70803940bed1b0b1759b62f100f4092f
Ismail Kılıç <ismail000728@gmail.com>
2022-02-05 00:55:25 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -6,27 +6,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      workbox: {
-        sourcemap: true
-      },
-      strategies: 'injectManifest',
-      manifest: {
-        name: 'TV Maze',
-        short_name: 'TV Maze',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),]
+    // VitePWA({
+    //   includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    //   workbox: {
+    //     sourcemap: true
+    //   },
+    //   strategies: 'injectManifest',
+    //   manifest: {
+    //     name: 'TV Maze',
+    //     short_name: 'TV Maze',
+    //     icons: [
+    //       {
+    //         src: '/android-chrome-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: '/android-chrome-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    // }),
+  ]
 })
