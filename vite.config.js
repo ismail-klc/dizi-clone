b74d44dcf289660ee146f4009c47372defda3e8a
Ismail Kılıç <ismail000728@gmail.com>
2022-02-07 19:45:32 +0300

    pwa removed.

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      strategies: 'injectManifest',
-      manifest: {
-        name: 'Dizi TV',
-        short_name: 'Dizi TV',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
   ]
 })
