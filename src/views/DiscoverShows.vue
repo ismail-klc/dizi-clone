19e1e3411959fbbbfa3e1053ca21287f55a22527
Ismail Kılıç <ismail000728@gmail.com>
2022-02-09 15:58:23 +0300

    discover page created.

---
+++ src/views/DiscoverShows.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>keşfet</div>
+</template>
+
+<script setup>
+import appAxios from "../utils/appAxios";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+onMounted(async () => {
+  console.log(router.currentRoute.value.query.p);
+});
+</script>
